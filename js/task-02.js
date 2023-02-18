ba5091393f83b0109063cfacf00787018377d6cc
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-02-18 19:40:45 +0100

    Completed second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.getElementById("ingredients");
+
+ingredients.forEach((element) => {
+  let li = document.createElement("li");
+  list.appendChild(li);
+  li.innerHTML = li.innerHTML + element;
+});
