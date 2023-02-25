9f54aefd851a9522c4c6db895dcad78fb37e58ee
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-02-25 02:57:43 +0100

    Added var to task eight

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,5 @@
+const email = document.querySelector("form.login-form input[name=email]");
+const pass = document.querySelector("form.login-form input[name=password]");
+const submit = document.querySelector("button[type=submit]");
 
+submit.addEventListener("click", () => {});
