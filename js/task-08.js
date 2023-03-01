d19b8a8ef1a028a58849db0a78aefb3152e2567d
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-01 15:36:44 +0100

    Completed eight task

--- js/task-08.js
+++ js/task-08.js
@@ -1,5 +1,26 @@
 const email = document.querySelector("form.login-form input[name=email]");
 const pass = document.querySelector("form.login-form input[name=password]");
 const submit = document.querySelector("button[type=submit]");
+const form = document.querySelector(".login-form");
 
-submit.addEventListener("click", () => {});
+function preventRefresh(event) {
+  event.preventDefault();
+}
+
+form.addEventListener("submit", preventRefresh);
+
+submit.addEventListener("click", () => {
+  if (email.value == "" || pass.value == "") {
+    alert("Wszystkie pola formularza powinny zostać uzupełnione!");
+    return;
+  }
+
+  let userCredentials = {
+    email: email.value,
+    password: pass.value,
+  };
+
+  console.log(userCredentials);
+
+  form.reset();
+});
