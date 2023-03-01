17af4768e6e638f3bb25cdeb85f45f22112bd49d
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-03-01 15:37:21 +0100

    Completed ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeColor = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+changeColor.addEventListener("click", () => {
+  let randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  spanColor.innerHTML = randomColor;
+});
